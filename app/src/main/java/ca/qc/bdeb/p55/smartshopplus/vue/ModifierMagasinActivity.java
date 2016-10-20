34fffe5db7261b19b0e7e28debf8e68251421f47
Catalin <catadeluxxxe@gmail.com>
2016-10-20 15:40:39 -0400

    Bouton image magasin Activity modification magasin marche bien Modification magasin fonctionne bien Certaines améliorations au niveau du vicabulaire utilisé dans menus et les toast

--- app/src/main/java/ca/qc/bdeb/p55/smartshopplus/vue/ModifierMagasinActivity.java
+++ app/src/main/java/ca/qc/bdeb/p55/smartshopplus/vue/ModifierMagasinActivity.java
@@ -1,19 +1,30 @@
 package ca.qc.bdeb.p55.smartshopplus.vue;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import ca.qc.bdeb.p55.smartshopplus.R;
 import ca.qc.bdeb.p55.smartshopplus.bd.DbHelper;
 import ca.qc.bdeb.p55.smartshopplus.modele.Magasin;
 
 public class ModifierMagasinActivity extends AppCompatActivity {
+
+    public final static int IMAGE_PICK = 7;
+    public static final int TAKE_PICTURE = 8;
 
     Magasin magasin;
     Long idMag;
@@ -23,6 +34,9 @@ public class ModifierMagasinActivity extends AppCompatActivity {
 
     EditText edtNomMagasin;
     ImageButton iBtnImageMagasin;
+
+    String imgDecodableString;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +55,58 @@ public class ModifierMagasinActivity extends AppCompatActivity {
         assignerVariablesAuxViews();
 
         peuplerViewsAvecDonneesProduit();
+
+        iBtnImageMagasin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                getIntent.setType("image/*");
+
+                Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                pickIntent.setType("image/*");
+
+                Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+                
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
+
+                startActivityForResult(chooserIntent, IMAGE_PICK);
+
+            }
+        });
+    }
+
+    // When a result is returned from another Activity onActivityResult is called.
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // First we need to check if the requestCode matches the one we used.
+        if (requestCode == IMAGE_PICK) {
+
+            // The resultCode is set by the DetailActivity
+            // By convention RESULT_OK means that what ever
+            // DetailActivity did was successful
+            if (resultCode == Activity.RESULT_OK) {
+                if (data != null) {
+                    // Get the result from the returned Intent
+                    try {
+                        InputStream inputStream = this.getContentResolver().openInputStream(data.getData());
+
+                        BitmapFactory.Options options = new BitmapFactory.Options();
+                        options.inSampleSize = 8;
+                        Bitmap preview_bitmap = BitmapFactory.decodeStream(inputStream, null, options);
+
+                        this.magasin.setImage(preview_bitmap);
+                        this.iBtnImageMagasin.setImageBitmap(this.magasin.getImage());
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -81,6 +147,7 @@ public class ModifierMagasinActivity extends AppCompatActivity {
     /**
      * Assigne les Views de la vue (fichier XML) aux variables d'instance
      */
+
     private void assignerVariablesAuxViews() {
         edtNomMagasin = (EditText) findViewById(R.id.activity_modifier_magasin_edt_nom_magasin);
         iBtnImageMagasin = (ImageButton) findViewById(R.id.activity_modifier_magasin_ibtn_image_magasin);
